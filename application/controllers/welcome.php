0cdbd39190858ce66ab8bfe2105b00b45691a818
Clara Wu <s.clara.wu@gmail.com>
2013-08-26 03:21:14 -0400

    controller, added friend functionality

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -1,6 +1,11 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 class Welcome extends CI_Controller {
+  function __construct() {
+    parent::__construct();
+    $this->load->helper('url');
+  }
+  
 
 	/**
 	 * Index Page for this controller.
@@ -18,9 +23,68 @@ class Welcome extends CI_Controller {
 	 * @see http://codeigniter.com/user_guide/general/urls.html
 	 */
 	public function index()
-	{
-		$this->load->view('welcome_message');
-	}
+  {
+    $this->load->library('facebook');
+
+    $user = $this->facebook->getUser();
+
+    if ($user) {
+      try {
+        $data['user_profile'] = $this->facebook->api('/me');
+        } catch (FacebookApiException $e) {
+          $user = null;
+        }
+    }
+
+    if ($user) {
+      $data['logout_url'] = $this->facebook->getLogoutUrl();
+    } else {
+      $data['login_url'] = $this->facebook->getLoginUrl();
+    }
+
+    $this->load->view('view',$data);
+  }
+
+    public function logout() {
+      $this->load->library('facebook');
+      session_destroy();
+      $this->facebook->destroySession();
+      redirect('/welcome');
+  }
+
+    public function friends() {
+      $this->load->library('facebook');
+
+      $user = $this->facebook->getUser();
+
+      if ($user) {
+        try {
+          if (!isset($_SESSION['score'])) {
+            $_SESSION['score'] = 0;
+            }
+            if (isset($_SESSION['friend_array'])) {
+              $this->friend_array = $_SESSION['friend_array'];
+            } else {
+              $this->full_friends = $this->facebook->api('/me/friends');
+              $_SESSION['friend_array'] =  $this->full_friends['data'];
+              $this->friend_array = $_SESSION['friend_array'];
+            }
+            $index_number = array_rand($this->friend_array, 1);
+            $data['user_profile'] = $this->friend_array[$index_number];
+            unset($this->friend_array[$index_number]);
+            $_SESSION['friend_array'] = array_values($this->friend_array);
+            $_SESSION['score'] = $_SESSION['score'] + 1;
+            $data['score'] = $_SESSION['score'];
+          } catch (FacebookApiException $e) {
+            $user = null;
+          }
+        echo json_encode($data);
+      }
+      else {
+        print "no";
+      }
+      
+    }
 }
 
 /* End of file welcome.php */

99dd484fd0abb27ab8bafce2e4d0986e61aa6c79
Clara Wu <s.clara.wu@gmail.com>
2013-08-26 04:33:21 -0400

    on empty, refresh image via ajax

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,30 @@
 $(document).ready(function() {
   if ($('#friends').length) {
     var guess = $('#guess_box').val();
-    $.ajax({
-      type : 'POST',
-      url : 'friends',
-      dataType : 'json',
-      data: {
-        'name' : guess
-      },
-      success : function(response){
-        $('#friends').append('<img src="' + response.url + '"/>'); 
-        $('#score').append('<h2>' + response.score + '</h2>');
+    guess_word('');
+    $('#guess_box').keypress(function(e) {
+      if (e.which == 13) {
+        $('#friends').empty();
+        $('#score').empty();
+        $('#guess_box').val('');
+        guess_word(guess);
+        return false;
       }
     });
+    function guess_word(guess) {
+      $.ajax({
+        type : 'POST',
+        url : 'friends',
+        dataType : 'json',
+        data: {
+          'name' : guess
+        },
+        success : function(response){
+          $('#friends').append('<img src="' + response.url + '"/>'); 
+          $('#score').append('<h2>' + response.score + '</h2>');
+        }
+      });
+    }
       
   }
 });
